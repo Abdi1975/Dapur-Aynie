d4ef7d4dbb96bfab0a6914cc9d434de4a161fc21
Ramadhan Abdillah <ramadhan.23002@mhs.unesa.ac.id>
2025-04-19 04:19:37 +0700

    koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,12 @@
 <?php  
+$host     = "192.168.1.10"; // IP Server Database
+$user     = "abdul";    // atau root
+$password = "123";   // sesuaikan
+$database = "restodapuraynie";
 
-date_default_timezone_set('ASIA/JAKARTA');
-$kon = mysqli_connect("localhost", "root", "", "restodapuraynie");
+$kon = mysqli_connect($host, $user, $password, $database);
 
+if (!$kon) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
 ?>
