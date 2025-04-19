e4c9bebe087267edf27542a86847fa8540732b57
Ramadhan Abdillah <ramadhan.23002@mhs.unesa.ac.id>
2025-04-19 19:03:05 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,4 @@
 <?php  
 date_default_timezone_set('Asia/Jakarta');
-$kon = mysqli_connect("localhost", "abdul", "123", "restodapuraynie");
+$kon = mysqli_connect("192.168.1.7", "admin", "123", "restodapuraynie");
 ?>
